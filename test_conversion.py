396fd60a7af20f006a15458101481039e7b88156
Michael Riffle <mriffle@uw.edu>
2022-04-28 14:12:27 -0700

    Update conversion test script

--- test_conversion.py
+++ test_conversion.py
@@ -17,6 +17,7 @@
 import os
 import requests
 import json
+import time
 from dotenv import load_dotenv
 
 # load values from .env into env
@@ -28,11 +29,21 @@ service_port = os.getenv('WEBAPP_PORT')
 test_json_str = os.getenv('CONVERSION_TEST_JSON')
 test_json_parsed = json.loads(test_json_str)
 
+test_project_id = os.getenv('TEST_PROJECT_ID')
+
+
 # create request
 url = 'http://' + service_host + ':' + service_port + '/requestNewBlibConversion'
 print('Sending request to:', url)
 response = requests.post(url, json=test_json_parsed)
-print('Got response:', json.loads(response.text))
+request_id = json.loads(response.text)
+print('Got request_id:', request_id)
 
+url = 'http://' + service_host + ':' + service_port + '/requestConversionStatus'
+request_dict = {'project_id': test_project_id, 'request_id': request_id}
 
+while True:
+    response = requests.post(url, json=request_dict)
+    print('Got status:', str(json.loads(response.text)))
 
+    time.sleep(10)
